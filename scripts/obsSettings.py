84e835cc4a246700d2440181c88b37d36c2e1fdd
The_Mach34 <masterjude5000@gmail.com>
2022-07-27 00:30:12 -0700

    Removed old LI code

--- scripts/obsSettings.py
+++ scripts/obsSettings.py
@@ -7,7 +7,6 @@ checks_per_second = 2  # Increase if missing operations, decrease if lagging
 scene_name_format = "MultiMC-"  # Edit this
 mc_source_format = "mc "  # Edit this
 wall_scene_name = "Wall"    # Edit this
-lock_indicator_format = "lock"  # Edit this if you use lock indicators
 
 # Single Scene Settings
 main_scene = "MainMulti"
