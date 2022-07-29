ed33400f3b4470086c7fdff53277dc0cca93820a
fikiskill <97172981+fikiskill@users.noreply.github.com>
2022-07-29 15:15:04 +0200

    Added ss startup

--- scripts/obsListener.py
+++ scripts/obsListener.py
@@ -19,6 +19,25 @@ last_completed_li_line = -1
 ops_file = "./scripts/obs.ops"
 li_file = "./scripts/li.ops"
 
+if sys.argv[2] == "True":
+    ssstartup()
+
+def ssstartup():
+    ws.call(requests.SetSceneItemProperties(
+            f"{config.wall_scene_name}", visible=False))
+    for num in range(1, instances + 1):
+        ws.call(requests.SetSceneItemProperties(
+            f"{config.mc_source_format}{num}", visible=True))
+        ws.call(requests.SetSceneItemProperties(
+            f"{config.bg_mc_source_format}{num}", visible=True))
+    for num in range(1, instances + 1):
+        time.sleep(1)
+        ws.call(requests.SetSceneItemProperties(
+            f"{config.mc_source_format}{num}", visible=False))
+        ws.call(requests.SetSceneItemProperties(
+            f"{config.bg_mc_source_format}{num}", visible=False))
+    ws.call(requests.SetSceneItemProperties(
+            f"{config.wall_scene_name}", visible=True))
 
 def tinderMotion(data):
     hide, show = data
